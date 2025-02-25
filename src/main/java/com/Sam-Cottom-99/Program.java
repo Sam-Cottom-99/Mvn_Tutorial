6001a21c8268e6f263e41ed24e193b667d391477
SCottom <sam.cottom@baxters.com>
2025-02-25 12:19:42 +0000

    Added Maven file structure, pom.xml and Program.java

---
+++ src/main/java/com/Sam-Cottom-99/Program.java
@@ -0,0 +1,7 @@
+package com.Sam-Cottom-99
+
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("This program is a Maven Project and the package is 'Sam-Cottom-99'");
+	}
+}
